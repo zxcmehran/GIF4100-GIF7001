4ba46bc9a68312b7f4306f5018205670d49a7607
Etienne <etienne.drouin.5@ulaval.ca>
2020-12-06 21:08:44 -0500

    Change to projection matrix

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ b = LaserDotDetectionV2.localisation(a[0], a[1], a[2], a[3], False)
 #    point = []
 points_L = np.array(b[0])#cv2.UMat(b[0])
 points_R = np.array(b[1])#cv2.UMat(b[1])
-stereoL = a[5]#cv2.UMat(a[5])
-stereoR = a[4]#cv2.UMat(a[4])
+stereoL = a[4]#cv2.UMat(a[5])
+stereoR = a[5]#cv2.UMat(a[4])
 R = a[6]
 T = a[7]
-mouvement_matrix = np.array([[R[0,0],R[0,1],R[0,2],T[0,0]],[R[1,0],R[1,1],R[1,2],T[1,0]],[R[2,0],R[2,1],R[2,2],T[2,0]]])
-stereoR = np.matmul(stereoR, mouvement_matrix)
-stereoL = np.matmul(stereoL, mouvement_matrix)
+#mouvement_matrix = np.array([[R[0,0],R[0,1],R[0,2],T[0,0]],[R[1,0],R[1,1],R[1,2],T[1,0]],[R[2,0],R[2,1],R[2,2],T[2,0]]])
+#empty_matrix = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0]])
+#stereoR = np.matmul(stereoR, mouvement_matrix)
+#stereoL = np.matmul(stereoL, empty_matrix)
 Point_3d = []
 for i in range(0, len(points_L)):
-    D_points = cv2.triangulatePoints(stereoL,stereoR,points_R[i],points_L[i])
+    D_points = cv2.triangulatePoints(stereoL,stereoR,points_L[i],points_R[i])
     homo = D_points[3,0]
     Point_3d.append([D_points[0,0]/homo,D_points[1,0]/homo,D_points[2,0]/homo])
 
