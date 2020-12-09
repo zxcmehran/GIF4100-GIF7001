4c622dd293621fa939556e466acaa31cfdfab5e5
GeoffreySawyerCharron <58184655+GeoffreySawyerCharron@users.noreply.github.com>
2020-12-09 15:13:44 -0500

    Add files via upload
    V4

---
+++ main.py
@@ -0,0 +1,40 @@
+import Calibration
+import LaserDotDetectionV2
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+rejected = 0
+
+a = Calibration.calibration(False)
+
+b = LaserDotDetectionV2.localisation(a[0], a[1], a[2], a[3], False)
+
+points_L = np.array(b[0])  # cv2.UMat(b[0])
+points_R = np.array(b[1])  # cv2.UMat(b[1])
+stereoL = a[4]  # cv2.UMat(a[5])
+stereoR = a[5]  # cv2.UMat(a[4])
+R = a[6]
+T = a[7]
+
+Point_3d = []
+for i in range(2, len(points_L)):
+    if (points_R[i][0] != 0 and points_R[i][1] != 0) or (points_L[i][0] != 0 and points_L[i][1] != 0):
+        D_points = cv2.triangulatePoints(stereoL, stereoR, points_L[i], points_R[i])
+        homo = D_points[3, 0]
+        Point_3d.append([D_points[0, 0]/homo, D_points[1, 0]/homo, D_points[2, 0]/homo])
+    else:
+        rejected += 1
+
+print("Points rejected: ", rejected)
+print("Press (q) to quit")
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+point_numpy = np.array(Point_3d)
+ax.scatter(point_numpy[:, 0], point_numpy[:, 1], point_numpy[:, 2])
+plt.show()
+
+if cv2.waitKey(1) & 0xff == ord('q'):
+    cv2.destroyAllWindows()
